152a69ea38141b91fe00123ee8d08279534a0116
Travis CI User <travis@example.org>
2017-10-31 14:59:03 +0000

    Documentation for rust-lang-nursery/net2-rs@86ce941

---
+++ implementors/std/sys/imp/ext/io/trait.AsRawFd.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["net2"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/sys/imp/ext/io/trait.AsRawFd.html\" title=\"trait std::sys::imp::ext::io::AsRawFd\">AsRawFd</a> for <a class=\"struct\" href=\"net2/struct.TcpBuilder.html\" title=\"struct net2::TcpBuilder\">TcpBuilder</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/sys/imp/ext/io/trait.AsRawFd.html\" title=\"trait std::sys::imp::ext::io::AsRawFd\">AsRawFd</a> for <a class=\"struct\" href=\"net2/struct.UdpBuilder.html\" title=\"struct net2::UdpBuilder\">UdpBuilder</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
